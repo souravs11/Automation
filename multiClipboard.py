019ad3a6012e91346f43a094ce270aa85424275c
Sourav <49033639+souravs11@users.noreply.github.com>
2022-07-23 13:50:01 +0530

    Update multiClipboard.py

--- multiClipboard.py
+++ multiClipboard.py
@@ -14,3 +14,19 @@ import json
 
 # Access 1-onwards the command-line arguments
 # print(sys.argv[1:])
+
+# Checking the number of arguments
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    # print(f'You entered: ', command)
+    # Checking which argument you entered
+    if command == 'save':
+        print(f'You entered: ',command)
+    elif command == 'load':
+        print(f'You entered: ',command)
+    elif command == 'list':
+        print(f'You entered: ',command)
+    else:
+        print(f'Unknown command!')
+else:
+    print(f'this command only accepts 1 command at a time.')
