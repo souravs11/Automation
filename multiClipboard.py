5e757d83221a9ec331aaf24f963130f1795c55e2
Sourav <49033639+souravs11@users.noreply.github.com>
2022-07-23 13:39:50 +0530

    Create multiClipboard.py

---
+++ multiClipboard.py
@@ -0,0 +1,16 @@
+import sys
+import clipboard
+import json
+
+# Access whatever is present on your clipboard
+# data = clipboard.paste()
+# print(data)
+
+# Write anything to the clipboard
+# clipboard.copy('abc')
+
+# Access all the command-line arguments
+# print(sys.argv)
+
+# Access 1-onwards the command-line arguments
+# print(sys.argv[1:])
